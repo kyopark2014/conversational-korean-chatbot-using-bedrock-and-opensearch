a9546c1381a0bd74a1c84b9e340e8ddebe2fa491
ksdyb.park <ksdyb@amazon.com>
2023-09-19 23:18:25 +0900

    update

---
+++ cdk-bedrock-chatbot-with-os/test/cdk-bedrock-chatbot-with-os.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBedrockChatbotWithOs from '../lib/cdk-bedrock-chatbot-with-os-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-bedrock-chatbot-with-os-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBedrockChatbotWithOs.CdkBedrockChatbotWithOsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
---
+++ lambda-chat/lambda_function.py
@@ -0,0 +1,390 @@
+import json
+import boto3
+import os
+import time
+import datetime
+from io import BytesIO
+import PyPDF2
+import csv
+import sys
+import re
+
+from langchain import PromptTemplate, SagemakerEndpoint
+from langchain.llms.sagemaker_endpoint import LLMContentHandler
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
+
+from langchain.agents import create_csv_agent
+from langchain.agents.agent_types import AgentType
+from langchain.llms.bedrock import Bedrock
+from langchain.chains.question_answering import load_qa_chain
+
+from langchain.vectorstores import FAISS
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.document_loaders import CSVLoader
+from langchain.embeddings import BedrockEmbeddings
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import OpenSearchVectorSearch
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+
+s3 = boto3.client('s3')
+s3_bucket = os.environ.get('s3_bucket') # bucket name
+s3_prefix = os.environ.get('s3_prefix')
+callLogTableName = os.environ.get('callLogTableName')
+bedrock_region = os.environ.get('bedrock_region')
+enableReference = os.environ.get('enableReference', 'false')
+
+# opensearch authorization - id/passwd
+opensearch_account = os.environ.get('opensearch_account')
+opensearch_passwd = os.environ.get('opensearch_passwd')
+opensearch_url = os.environ.get('opensearch_url')
+
+modelId = os.environ.get('model_id')
+print('model_id: ', modelId)
+isReady = False   
+    
+# supported llm list from bedrock
+boto3_bedrock = boto3.client(
+    service_name='bedrock',
+    region_name=bedrock_region,
+)
+
+parameters = {
+    "max_tokens_to_sample":1024,
+}
+HUMAN_PROMPT = "\n\nHuman:"
+AI_PROMPT = "\n\nAssistant:"
+
+llm = Bedrock(model_id=modelId, client=boto3_bedrock, model_kwargs=parameters)
+
+map = dict()  # Conversation
+
+# embedding
+bedrock_embeddings = BedrockEmbeddings(
+    client=boto3_bedrock,
+    region_name = bedrock_region,
+    model_id = 'amazon.titan-embed-g1-text-02' # amazon.titan-e1t-medium
+)
+
+# load documents from s3 for pdf and txt
+def load_document(file_type, s3_file_name):
+    s3r = boto3.resource("s3")
+    doc = s3r.Object(s3_bucket, s3_prefix+'/'+s3_file_name)
+    
+    if file_type == 'pdf':
+        contents = doc.get()['Body'].read()
+        reader = PyPDF2.PdfReader(BytesIO(contents))
+        
+        raw_text = []
+        for page in reader.pages:
+            raw_text.append(page.extract_text())
+        contents = '\n'.join(raw_text)    
+        
+    elif file_type == 'txt':        
+        contents = doc.get()['Body'].read().decode('utf-8')
+        
+    print('contents: ', contents)
+    new_contents = str(contents).replace("\n"," ") 
+    print('length: ', len(new_contents))
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100,
+        separators=["\n\n", "\n", ".", " ", ""],
+        length_function = len,
+    ) 
+
+    texts = text_splitter.split_text(new_contents) 
+    print('texts[0]: ', texts[0])
+    
+    return texts
+
+# load csv documents from s3
+def load_csv_document(s3_file_name):
+    s3r = boto3.resource("s3")
+    doc = s3r.Object(s3_bucket, s3_prefix+'/'+s3_file_name)
+
+    lines = doc.get()['Body'].read().decode('utf-8').split('\n')   # read csv per line
+    print('lins: ', len(lines))
+        
+    columns = lines[0].split(',')  # get columns
+    print('columns: ', columns)
+    
+    docs = []
+    n = 0
+    for row in csv.DictReader(lines, delimiter=',',quotechar='"'):
+        values = {k: row[k] for k in columns if k in row}
+        content = "\n".join(f"{k.strip()}: {v.strip()}" for k, v in values.items())
+        doc = Document(
+            page_content=content,
+            metadata={
+                'name': s3_file_name,
+                'row': n+1,
+            }
+        )
+        docs.append(doc)
+        n = n+1
+    print('docs[0]: ', docs[0])
+
+    return docs
+
+def get_summary(texts):    
+    # check korean
+    pattern_hangul = re.compile('[\u3131-\u3163\uac00-\ud7a3]+') 
+    word_kor = pattern_hangul.search(str(texts))
+    print('word_kor: ', word_kor)
+    
+    if word_kor:
+        #prompt_template = """\n\nHuman: 다음 텍스트를 간결하게 요약하세오. 텍스트의 요점을 다루는 글머리 기호로 응답을 반환합니다.
+        prompt_template = """\n\nHuman: 다음 텍스트를 요약해서 500자 이내로 설명하세오.
+
+        {text}
+        
+        Assistant:"""        
+    else:         
+        prompt_template = """\n\nHuman: Write a concise summary of the following:
+
+        {text}
+        
+        Assistant:"""
+    
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+    chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
+
+    docs = [
+        Document(
+            page_content=t
+        ) for t in texts[:3]
+    ]
+    summary = chain.run(docs)
+    print('summary: ', summary)
+
+    if summary == '':  # error notification
+        summary = 'Fail to summarize the document. Try agan...'
+        return summary
+    else:
+        # return summary[1:len(summary)-1]   
+        return summary
+
+def get_answer_using_template_with_history(query, vectorstore, chat_memory):  
+    # check korean
+    pattern_hangul = re.compile('[\u3131-\u3163\uac00-\ud7a3]+') 
+    word_kor = pattern_hangul.search(str(query))
+    print('word_kor: ', word_kor)
+    
+    if word_kor:
+        condense_template = """\n\nHuman: 다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant는 모르는 질문을 받으면 솔직히 모른다고 말합니다.
+    
+        {chat_history}
+        
+        Human: {question}
+
+        Assistant:"""
+    else:
+        condense_template = """Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer. You will be acting as a thoughtful advisor.
+        
+        {chat_history}
+        
+        Human: {question}
+
+        Assistant:"""
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_template)     
+        
+    # extract chat history
+    chats = chat_memory.load_memory_variables({})
+    chat_history_all = chats['history']
+    print('chat_history_all: ', chat_history_all)
+
+    # use last two chunks of chat history
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=0,
+        separators=["\n\n", "\n", ".", " ", ""],
+        length_function = len)
+    texts = text_splitter.split_text(chat_history_all) 
+
+    pages = len(texts)
+    print('pages: ', pages)
+
+    if pages >= 2:
+        chat_history = f"{texts[pages-2]} {texts[pages-1]}"
+    elif pages == 1:
+        chat_history = texts[0]
+    else:  # 0 page
+        chat_history = ""
+    
+    # load related docs
+    relevant_documents = vectorstore.similarity_search(query)
+    #print('relevant_documents: ', relevant_documents)
+
+    print(f'{len(relevant_documents)} documents are fetched which are relevant to the query.')
+    print('----')
+    for i, rel_doc in enumerate(relevant_documents):
+        body = rel_doc.page_content[rel_doc.page_content.rfind('Document Excerpt:')+18:len(rel_doc.page_content)]
+        # print('body: ', body)
+        
+        chat_history = f"{chat_history}\nHuman: {body}"  # append relevant_documents 
+        print(f'## Document {i+1}: {rel_doc.page_content}')
+        print('---')
+
+    print('chat_history:\n ', chat_history)
+
+    # make a question using chat history
+    if pages >= 1:
+        result = llm(CONDENSE_QUESTION_PROMPT.format(question=query, chat_history=chat_history))
+    else:
+        result = llm(HUMAN_PROMPT+query+AI_PROMPT)
+    # print('result: ', result)
+
+    # add refrence
+    if len(relevant_documents)>=1 and enableReference=='true':
+        reference = get_reference(relevant_documents)
+        # print('reference: ', reference)
+
+        return result+reference
+    else:
+        return result    
+
+def get_reference(docs):
+    reference = "\n\nFrom\n"
+    for doc in docs:
+        name = doc.metadata['name']
+        page = doc.metadata['page']
+    
+        reference = reference + (str(page)+'page in '+name+'\n')
+    return reference
+
+def lambda_handler(event, context):
+    print(event)
+    userId  = event['user-id']
+    print('userId: ', userId)
+    requestId  = event['request-id']
+    print('requestId: ', requestId)
+    type  = event['type']
+    print('type: ', type)
+    body = event['body']
+    print('body: ', body)
+
+    global modelId, llm, vectorstore, isReady, map, qa
+    global enableConversationMode, enableReference, enableRAG  # debug
+    
+    # memory for conversation
+    if userId in map:
+        chat_memory = map[userId]
+        print('chat_memory exist. reuse it!')
+    else: 
+        chat_memory = ConversationBufferMemory(human_prefix='Human', ai_prefix='Assistant')
+        map[userId] = chat_memory
+        print('chat_memory does not exist. create new one!')
+    
+    vectorstore = OpenSearchVectorSearch(
+        index_name = "rag-index-*", # all
+        #index_name = 'rag-index-'+userId+'-*',
+        is_aoss = False,
+        #engine="faiss",  # default: nmslib
+        embedding_function = bedrock_embeddings,
+        opensearch_url=opensearch_url,
+        http_auth=(opensearch_account, opensearch_passwd), # http_auth=awsauth,
+    )
+    start = int(time.time())    
+
+    if type == 'text':
+        text = body
+        print('query: ', text)
+            
+        # debugging
+        if text == 'enableReference':
+            enableReference = 'true'
+            msg  = "Referece is enabled"
+        elif text == 'disableReference':
+            enableReference = 'false'
+            msg  = "Reference is disabled"
+        else:
+            querySize = len(text)
+                    
+            if querySize<1800: 
+                msg = get_answer_using_template_with_history(text, vectorstore, chat_memory)
+                                                              
+                storedMsg = str(msg).replace("\n"," ") 
+                chat_memory.save_context({"input": text}, {"output": storedMsg})
+                            
+            else:
+                msg = llm(HUMAN_PROMPT+text+AI_PROMPT)
+            
+    elif type == 'document':
+        object = body
+        
+        file_type = object[object.rfind('.')+1:len(object)]
+        print('file_type: ', file_type)
+            
+        # load documents where text, pdf, csv are supported
+        if file_type == 'csv':
+            docs = load_csv_document(object)
+
+            texts = []
+            for doc in docs:
+                texts.append(doc.page_content)
+            print('texts: ', texts)
+
+        else:
+            texts = load_document(file_type, object)
+
+            docs = []
+            for i in range(len(texts)):
+                docs.append(
+                    Document(
+                        page_content=texts[i],
+                        metadata={
+                            'name': object,
+                            'page':i+1
+                        }
+                    )
+                )
+            print('docs[0]: ', docs[0])    
+            print('docs size: ', len(docs))
+
+        new_vectorstore = OpenSearchVectorSearch(
+            index_name="rag-index-"+userId+'-'+requestId,
+            is_aoss = False,
+            #engine="faiss",  # default: nmslib
+            embedding_function = bedrock_embeddings,
+            opensearch_url = opensearch_url,
+            http_auth=(opensearch_account, opensearch_passwd),
+        )
+        new_vectorstore.add_documents(docs)
+
+        # summerize the document
+        msg = get_summary(texts)
+                
+    elapsed_time = int(time.time()) - start
+    print("total run time(sec): ", elapsed_time)
+
+    print("Human: ", text)
+    print('Assistant: ', msg)
+
+    item = {
+        'user-id': {'S':userId},
+        'request-id': {'S':requestId},
+        'type': {'S':type},
+        'body': {'S':body},
+        'msg': {'S':msg}
+    }
+
+    client = boto3.client('dynamodb')
+    try:
+        resp =  client.put_item(TableName=callLogTableName, Item=item)
+    except: 
+        raise Exception ("Not able to write into dynamodb")
+        
+    print('resp, ', resp)
+
+    return {
+        'statusCode': 200,
+        'msg': msg,
+    }
